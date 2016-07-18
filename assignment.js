b7736d61649eec4faeffe2f968ddb30503ed2519
miyamae <b5005@oic.jp>
2016-07-18 23:44:23 +0900

    JSONファイルのサンプル用意

--- assignment.js
+++ assignment.js
@@ -2,7 +2,7 @@ function loadSection(num) {
     $.getJSON("practice_" + num + ".json", function(data){
       $(".mdl-layout__content").empty();
         for(var i in data){
-        $(".mdl-layout__content").append("<div class='e_card mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect'>" + data[i].e_word + "<p>" + data[i].j_word + "</p></div>");
+        $(".mdl-layout__content").append("<div class='e_card mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect'><p>" + data[i].e_word + "</p><p>" + data[i].j_word + "</p></div>");
         componentHandler.upgradeDom();
         }
     });

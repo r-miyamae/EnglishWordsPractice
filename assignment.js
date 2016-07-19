16a0c409c502ab47f5122f9f75a3a3dbc6d0f267
miyamae <b5005@oic.jp>
2016-07-19 23:33:30 +0900

    単語カードの調整 インポート・エクスポート処理をやろうかと思った（思っただけ）

--- assignment.js
+++ assignment.js
@@ -2,15 +2,32 @@ function loadSection(num) {
     $.getJSON("practice_" + num + ".json", function(data){
       $(".mdl-layout__content").empty();
         for(var i in data){
-        $(".mdl-layout__content").append("<div class='e_card mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect'><p>" + data[i].e_word + "</p><p>" + data[i].j_word + "</p></div>");
+        $(".mdl-layout__content").append("<div class='e_card mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect'><p class='e_w'>"
+                                          + data[i].e_word + "</p><p class='j_w'>" + data[i].j_word + "</p></div>");
         componentHandler.upgradeDom();
         }
     });
-  };
+};
+
+function createJson(){
+  $(".mdl-layout__content").empty();
+  $(".mdl-layout__content").append("<div class='j_form'></div>");
+  componentHandler.upgradeDom();
+}
+
 function dFocus(){
   $('.mdl-layout__obfuscator').trigger("click");
 };
 
 $(document).ready(function(){
   loadSection(1);
+});
+
+$('.e_card').live('click',function(){
+
+  if ($(".j_w",this).is(':hidden')){
+    $(".j_w",this).css("display","block");
+  }else{
+    $(".j_w",this).css("display","none");
+  }
 });

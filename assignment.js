97d7e372e4e04c930020641830abd517bbf30510
miyamae <b5005@oic.jp>
2016-10-18 13:19:33 +0900

    Import機能の実装

--- assignment.js
+++ assignment.js
@@ -1,17 +1,25 @@
-function loadSection(num) {
-    $.getJSON("practice_" + num + ".json", function(data){
-      $(".mdl-layout__content").empty();
-        for(var i in data){
-        $(".mdl-layout__content").append("<div class='e_card mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect'><p class='e_w'>"
-                                          + data[i].e_word + "</p><p class='j_w'>" + data[i].j_word + "</p></div>");
-        componentHandler.upgradeDom();
-        }
-    });
-};
+function loadSection(name) {//jsonファイルを基にカードを生成
+  $.getJSON(name, function　createCard(data){
+    $(".mdl-layout__content").empty();
+      for(var i in data){
+      $(".mdl-layout__content").append("<div class='e_card mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect'><p class='e_w'>"
+                                        + data[i].e_word + "</p><p class='j_w'>" + data[i].j_word + "</p></div>");
+      componentHandler.upgradeDom();
+      }
+  });
+}
 
-function createJson(){
+function　createCard(data){//データを直接渡しカードを生成
   $(".mdl-layout__content").empty();
-  // $(".mdl-layout__content").append("<div class='j_form'><p>未実装です</p></div>");
+    for(var i in data){
+    $(".mdl-layout__content").append("<div class='e_card mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect'><p class='e_w'>"
+                                      + data[i].e_word + "</p><p class='j_w'>" + data[i].j_word + "</p></div>");
+    componentHandler.upgradeDom();
+    }
+  }
+
+function createJson(){//Jsonファイルを作成
+
   var data = {  //このdataに突っ込んでください♡
     "a": "aaa",
     "b": "bbb"
@@ -20,7 +28,7 @@ function createJson(){
   var blob = new Blob([data], {type : "text/json"});
   var file_title;
  // if(document.querySelector('#textbox_1').value != ''){　//textbox_1に何も入力されていないと実行できないようにするif文
-    file_title = "test" //ここを書き換えることでダウンロードリンクのタイトルが変わる
+    file_title = "your_english_words" //ここを書き換えることでダウンロードリンクのタイトルが変わる
 // }
  // if(file_title != undefined){  //ファイルタイトルが何も入力されていなかった場合、実行できなくする
     if(window.navigator.msSaveBlob){
@@ -30,11 +38,11 @@ function createJson(){
     else
     {
       window.URL = window.URL || window.webkitURL;
-      var links = document.querySelector('.mdl-navigation');
+      var links = document.querySelector(".form");
       var temp = document.createElement("a");
       temp.innerHTML = file_title;
       temp.href = window.URL.createObjectURL(blob);
-      temp.setAttribute("class", "download_link")
+      temp.setAttribute("class", "download_link");
       temp.setAttribute("download", file_title);
       links.appendChild(temp);
     }
@@ -42,19 +50,63 @@ function createJson(){
   // componentHandler.upgradeDom();
 }
 
-function loadJson() {
+function createForm(){
+    $(".mdl-layout__content").empty();
+    // var data= '[\
+    //             {\
+    //               "e_word":"respect…" ,\
+    //               "j_word":"…を尊敬する"\
+    //             },\
+    //             {\
+    //                "e_word":"will" ,\
+    //                "j_word":"意思"\
+    //             },\
+    //             {\
+    //               "e_word":"individual" ,\
+    //               "j_word":"個人"\
+    //             },\
+    //             {\
+    //               "e_word":"Take it easy.",\
+    //               "j_word":"気楽にやれ"\
+    //             },\
+    //             {\
+    //               "e_word":"assure A (that) ~",\
+    //               "j_word":"Aに~を確約する"\
+    //             },\
+    //             {\
+    //               "e_word":"turn out (to be)…",\
+    //               "j_word":"…という結果になる"\
+    //             }\
+    //           ]'
+    $(".mdl-layout__content").append('<div class="form">以下に英語とそれの日本語訳を記入し自分のフラッシュカードを作成できます<br>\
+                                        <textarea name="japanese" id="japanese" rows="25"></textarea>\
+                                        <textarea name="english" id="english" rows="25"></textarea>\
+                                        </br>\
+                                        <a href="#" onClick=createJson()>ダウンロードリンクの生成</button></div>');
+    componentHandler.upgradeDom();
+}
+
+
+function chooseJson() {
+  $(".mdl-layout__content").empty();
+  $(".mdl-layout__content").append('<div class="form"><input type="file" name="name" value="" id="file_1">\
+                                    <a href="#" onClick="loadJson();">決定</a></div>');
+  componentHandler.upgradeDom();
+}
+
+function loadJson(){
   var inp_file = document.querySelector('#file_1').files[0];  //ファイルフォームのidをここに指定
   var reader = new FileReader();
   reader.addEventListener('load',function (e){
     var file_data = reader.result;
     file_data = JSON.parse(file_data);
-
+    createCard(file_data);
     //オブジェクトを配列に書き換える処理なので、いるなら使ってやってください
-    //file_data = $.map(file_data, function(val, key) { return val; });
+    // file_data = $.map(file_data, function(val, key) { return val; });
 
     //これでfile_dataにオブジェクトが追加されてるんで、このカッコの中で画面に描画する処理をしてください。
 
-    console.log(file_data); //ほら。読めてるでしょ？
+    // console.log(file_data); //ほら。読めてるでしょ？
 
   }, true);
   reader.readAsText(inp_file);
@@ -65,7 +117,7 @@ function dFocus(){
 };
 
 $(document).ready(function(){
-  loadSection(1);
+  loadSection('practice_1.json');
 });
 
 $('.e_card').live('click',function(){
